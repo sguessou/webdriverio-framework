d2648a15a03811b442dd0f0e2bc02aa1e04f8ccb
Saad Guessous <guessous_saad@hotmail.com>
2019-08-12 20:07:13 +0300

    added test to showcase the waitUntil command

--- test/cart.js
+++ test/cart.js
@@ -37,12 +37,40 @@ describe("Cart Functionality", () => {
             let isBuyNowTextExisting = browser.$("button=Buy Now").isExisting();
             expect(isBuyNowTextExisting, "'buy now' should be disabled after completing purchase").to.be.false;
 
-            // waitForText on button
+            // waitForDisplayed on button
             browser.$("button=Buy Now").waitForDisplayed(3000);
 
             // Verify "buy now" is still disabled
             let isBtnEnabled = browser.$("#buyNowButton").isEnabled();
             expect(isBtnEnabled, "'buy now' should be disabled after completing purchase").to.be.false;
+        });
+
+        it('should clear input after completion', () => {
+            // waitForDisplayed for qty input
+            browser.$("#qty").waitUntil(() => {
+                return browser.$("#qty").getValue() === '';
+            }, 3000, 'Expected qty value to be empty after 3s');
+        });
+
+        it('should hide thank you message after clicking close button', () => {
+            // waitForExist "thank you message"
+            browser.$(".callout*=Thank you human").waitForExist(3000);
+
+            // click close button
+            browser.$(".close-button").click();
+
+            // check the style of text after it has disappeared
+           browser.$(".callout").waitUntil(() => {
+                return browser.$(".callout").getCSSProperty("display").value === "none";
+            }, 3000, "Expected display style of text to be 'none'");
+        });
+
+        it('should reset button text after purchase completes', () => {
+            // Verify button now says 'buy now'
+            let btnText = browser.$("#buyNowButton").getText();
+            browser.$("#buyNowButton").waitUntil(() => {
+                return browser.$("#buyNowButton").getText() === "Buy Now";
+            }, 3000, 'Button text should change to `Buy Now`');
         });
     });
 });
